3c053ac9bb6995ce855fd8c7660c327836361d7e
Ali <chatiraqe@gmail.com>
2026-05-14 11:22:35 +0300

    add basic template engine

---
+++ template_engine.py
@@ -0,0 +1,13 @@
+﻿from pathlib import Path
+
+TEMPLATES_DIR = Path(__file__).parent / "templates"
+
+def list_templates():
+    if not TEMPLATES_DIR.exists():
+        return []
+
+    return [
+        item.name
+        for item in TEMPLATES_DIR.iterdir()
+        if item.is_file()
+    ]
